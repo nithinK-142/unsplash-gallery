fc6c6187c3c4fa9ab4213fb75a232102b57c0ee3
nithinK-142 <nithin.sagar.359@gmail.com>
2023-08-18 10:21:13 +0530

    Preloading images to reduce the delay when previous and next are clicked.

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,20 +1,19 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { ImageContext } from '../App';
-import right from "../assets/arrow-right.svg";
-import left from "../assets/arrow-left.svg";
+import React, { useState, useContext, useEffect } from "react";
+import { ImageContext } from "../App";
+import next from "../assets/arrow-right.svg";
+import previous from "../assets/arrow-left.svg";
 
 const ImageSlider = ({ currentId, handleImageClick }) => {
-
   const { response } = useContext(ImageContext);
   const [jsonData, setJsonData] = useState([]);
   const [currentImage, setCurrentImage] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [screenSize, setScreenSize] = useState('w-3/4 h-3/4');
+  const [screenSize, setScreenSize] = useState("w-3/4 h-3/4");
 
   useEffect(() => {
     const formattedData = response.map((data) => ({
       id: data.id,
-      regularUrl: data.urls.regular
+      regularUrl: data.urls.regular,
     }));
     setJsonData(formattedData);
 
@@ -39,18 +38,36 @@ const ImageSlider = ({ currentId, handleImageClick }) => {
     updateImageAndIndex(currentIndex - 1);
   };
 
-  const switchScreenMode = () => {
-    setScreenSize(screenSize === 'w-3/4 h-3/4' ? 'w-full h-full' : 'w-3/4 h-3/4');
+  const preloadImage = (imageUrl) => {
+    const img = new Image();
+    img.src = imageUrl;
   };
-  
-  
+
+  useEffect(() => {
+    if (jsonData.length > 0) {
+      preloadImage(jsonData[(currentIndex + 1) % jsonData.length].regularUrl);
+      preloadImage(
+        jsonData[(currentIndex - 1 + jsonData.length) % jsonData.length]
+          .regularUrl
+      );
+    }
+  }, [jsonData, currentIndex]);
+
+  const switchScreenMode = () => {
+    setScreenSize(
+      screenSize === "w-3/4 h-3/4" ? "w-full h-full" : "w-3/4 h-3/4"
+    );
+  };
+
   const openImageInNewTab = () => {
-    window.open(currentImage, '_blank');
+    window.open(currentImage, "_blank");
   };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-70">
-      <div className={`${screenSize} m-auto relative group bg-slate-500 rounded-lg`}>
+      <div
+        className={`${screenSize} m-auto relative group bg-slate-500 rounded-lg`}
+      >
         <button
           className="absolute top-2 right-4 text-white text-lg"
           onClick={handleImageClick}
@@ -70,19 +87,17 @@ const ImageSlider = ({ currentId, handleImageClick }) => {
           {/* &#x1F855; arrow */}
           &#x25F3;
         </button>
-        <button 
-        className="absolute left-2 top-1/2" 
-        onClick={handlePreviousImage}>
-          <img 
-          className="h-8"
-          src={left} alt="left" />
+        <button
+          className="absolute left-2 top-1/2"
+          onClick={handlePreviousImage}
+        >
+          <img className="h-8" src={previous} alt="previous" />
         </button>
-        <button 
-        className="absolute right-2 top-1/2" 
-        onClick={handleNextImage}>
-          <img 
-          className="h-8"
-          src={right} alt="right" />
+        <button
+          className="absolute right-2 top-1/2"
+          onClick={handleNextImage}
+        >
+          <img className="h-8" src={next} alt="next" />
         </button>
         <div
           style={{ backgroundImage: `url(${currentImage})` }}
