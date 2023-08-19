38b477981e5a38986e11daa2e4655005670ce780
nithinK-142 <nithin.sagar.359@gmail.com>
2023-08-19 20:04:41 +0530

    Remove: div width.

--- src/components/SliderContent.jsx
+++ src/components/SliderContent.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const SliderContent = ({ currentImage }) => {
   return (
-    <div
-      style={{ backgroundImage: `url(${currentImage})` }}
-      className="w-full h-full rounded-2xl bg-center bg-contain bg-no-repeat"
-    ></div>
+    // <div className="h-full bg-contain">
+      <div
+        style={{ backgroundImage: `url(${currentImage})` }}
+        className="h-full rounded-2xl bg-center bg-no-repeat bg-contain"
+      ></div>
+    // </div>
   );
 };
 
