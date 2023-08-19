a49a0512ce3e66dc65c59020ca278f3284c89f88
nithinK-142 <nithin.sagar.359@gmail.com>
2023-08-19 11:32:11 +0530

    Added hyperlink to h1(Unsplash Gallery).

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,9 @@ const NavBar = ({ children }) => {
   return (
     <div className="bg-[#5F746F] py-10 flex items-center">
       <div className="max-w-md mx-auto w-full">
-        <h1 className='text-white text-center text-2xl font-bold mb-5'>Unsplash Gallery</h1>
+        <a href="https://unsplashgalleryapp.netlify.app/">
+          <h1 className='text-white text-center text-2xl font-bold mb-5'>Unsplash Gallery</h1>
+        </a>
         { children }
       </div>
     </div>
